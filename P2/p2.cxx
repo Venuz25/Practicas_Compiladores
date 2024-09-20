aae6f797165916f51b64c51f17486bcb7c42e2fb
LuisACarrillo21 <langel.carrillo@gmail.com>
2024-09-20 11:03:06 -0600

    Programa

--- P2/p2.cxx
+++ P2/p2.cxx
@@ -7,7 +7,30 @@
 
 using namespace std;
 
+struct Lexema
+{
+    string token;
+    string lexema;
+    string valAtri;
+};
+
 int main(){
-    
+    ifstream archivo("Text.txt");
+    if (!archivo)
+    {
+        cerr << "No se pudo abrir el archivo." << endl;
+        return 1;
+    }
+
+    vector<Lexema> lexemas;
+    string palabra, linea;
+    size_t position;
+
+    cout << "TOKEN\t\t\t|\t\t\tLEXEMA\t\t\t|\t\t\tVALOR DE ATRIBUTO\n";
+    cout << "--------------------------------\n";
+    for (const auto &lex : lexemas)
+    {
+        cout << lex.token << "\t\t" << lex.lexema << endl;
+    }
     return 0;
 }

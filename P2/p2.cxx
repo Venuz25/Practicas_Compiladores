d67819a344f772cabb08aacaf0959c0882bf3505
LuisACarrillo21 <langel.carrillo@gmail.com>
2024-09-20 10:50:49 -0600

    practica_2

---
+++ P2/p2.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <regex>
+
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
---
+++ pruba.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int _2MAL;
+
+    _2MAL = 32;
+    printf("%d + %d = %d",_2MAL, _2MAL, _2MAL+_2MAL);
+    return 0;
+}
